5e4cb53aa0ff7964c1ff6d26f243174ac5f3d242
Daniel Cortez <dacortez79@gmail.com>
2012-06-03 22:47:30 -0300

    Refatoração do código

--- simplex.m
+++ simplex.m
@@ -1,74 +1,52 @@
 function [ind x] = simplex(A, b, c, m, n, print)
 	
-	# Fase 1 ##########################################################
+	# Resolve a fase 1 do problema.
+	[T B m] = phase_1(A, b, m, n, print);
+	
+	# Testa se o problema é inviável.
+	if T(1, 1) > 0
+		ind = +1
+		x = []
+		return;
+	end
+	
+	# Problema é viável, então continua com a fase 2.
+	[ind x] = phase_2(T, B, c, m, n, print);
+endfunction 
+
+
+function [T B m ind] = phase_1(A, b, m, n, print)
+	
+	printf("Simplex: Fase 1\n\n");
 	
 	# Coloca no formato b >= 0.
 	[A b] = make_b_positive(A, b, m, n);
-	
-	# Vetor de custos da fase 1. 
-	c1(1:n) = zeros(1, n);
-	c1(n+1:n+m) = ones(1, m);
 	
 	# Matriz A com variáveis artificiais.
 	A(1:m, n+1:m+n) = eye(m);
 	
 	# Tableau inicial para a fase 1.
-	T(1, 1) = -sum(b);
-	T(1, 2:n+1) = -sum(A(:, 1:n));
-	T(1, n+2:n+m+1) = zeros(1, m);
-	T(2:m+1, 1) = b;
-	T(2:m+1, 2:m+n+1) = A;
+	T = phase_1_tableau(A, b, m, n);
 	
-	# Vetor de índices das variáveis básicas.
-	b_idx = n+1:n+m;
+	# Vetor com os índices das variáveis básicas.
+	B = n+1:n+m;
 	
 	# Itera o tableau até encontrar solução.
-	[T b_idx ind x] = tableau(T, b_idx, m, m + n, print);
+	[T B] = tableau_solve(T, B, m, m + n, print, 1);
 		
 	# Testa se o problema é viável.
-	if T(1, 1) > 0
-		ind = +1;
-		x = [];
-		printf("Problema inviável.\n");
-		return;
-	endif
-	
-	# Elimina variáveis artificiais na solução básica da fase 1.
-	[T b_idx m] = remove_artificial(T, b_idx, m, n);
-	
-	# Imprime tableau final da fase 1.
-	if print
-		print_tableau(T, b_idx, m, n, 0, 0);
-	endif
-	
-	# Fase 2 ##########################################################
-	
-	# Calcula linha 1 do tableau inicial da fase 2.
-	c_B = c(b_idx);
-	T(1, 1) = -c_B' * T(2:m+1, 1);
-	T(1, 2:n+1) = c' - c_B' * T(2:m+1, 2:n+1);
-	
-	# Itera o tableau até encontrar solução.
-	[T b_idx ind x] = tableau(T, b_idx, m, n, print);
+	if T(1, 1) == 0
+		
+		# Remove colunas das variáveis artificiais do tableau.
+		T(:, n+2:n+m+1) = [];
 
-	# Imprime tableau final da fase 2.
-	if print
-		print_tableau(T, b_idx, m, n, 0, 0);
-	endif
+		# Remove restrições redundantes.
+		[T B m] = remove_redundants(T, B, m, n);
 
-	# Solução ilimitada (custo = -Inf)
-	if ind == -1
-		printf("Solução ilimitada.\n");
-	elseif ind == 0
-		printf("Solução ótima encontrada:\n");
-		printf("Custo = %2.4f\n", -T(1, 1));
-		for i = 1:n
-			printf("x%d = %2.4f\n", i, x(i));
-		endfor 
-		printf("\n");
+		# Retira variáveis artificiais da solução básica encontrada.
+		[T B] = exit_artificials(T, B, m, n);
 	endif
-	
-endfunction 
+endfunction
 
 
 function [A b] = make_b_positive(A, b, m, n)
@@ -81,14 +59,77 @@ function [A b] = make_b_positive(A, b, m, n)
 endfunction
 
 
-function [T b_idx ind x] = tableau(T, b_idx, m, n, print)
+function T = phase_1_tableau(A, b, m, n)
+	T(1, 1) = -sum(b);
+	T(1, 2:n+1) = -sum(A(:, 1:n));
+	T(1, n+2:n+m+1) = zeros(1, m);
+	T(2:m+1, 1) = b;
+	T(2:m+1, 2:m+n+1) = A;
+endfunction
+
+
+function [T B m] = remove_redundants(T, B, m, n)
+	remove_idx = []; 
+	for i = 1:m
+		if B(i) > n
+			if is_zero_vector(T(i + 1, 2:n+1))
+				remove_idx(end + 1) = i;
+			endif
+		endif
+	endfor
+	B(remove_idx) = [];
+	T(remove_idx + 1, :) = [];
+	m -= length(remove_idx);
+endfunction
+
+
+function [T B] = exit_artificials(T, B, m, n)
+	for i = 1:m
+		if B(i) > n
+			for j = 1:n
+				if T(i + 1, j + 1) != 0
+					T = pivot(T, i + 1, j + 1, m + 1, n + 1);
+					B(i) = j;
+				endif
+			endfor
+		endif
+	endfor
+endfunction
+	
+
+function bool = is_zero_vector(v)
+	for i = 1:length(v)
+		if v(i) != 0
+			bool = false;
+			return;
+		endif
+	endfor
+	bool = true;
+endfunction
+
+
+function [ind x] = phase_2(T, B, c, m, n, print)
+
+	printf("Simplex: Fase 2\n\n");
+
+	# Calcula linha 1 do tableau inicial da fase 2.
+	c_B = c(B);
+	T(1, 1) = -c_B' * T(2:m+1, 1);
+	T(1, 2:n+1) = c' - c_B' * T(2:m+1, 2:n+1);
+
+	# Itera o tableau até encontrar solução.
+	[T B ind x] = tableau_solve(T, B, m, n, print, 1);
+endfunction
+
+
+function [T B ind x] = tableau_solve(T, B, m, n, print, iter)
 
 	while true
 		
 		# Marca posição do pivot.
 		i_pivot = j_pivot = 0;
 	
-		# Procura candidato a entrar na base.
+		# Procura variável candidato a entrar na base.
 		for j = 2:n+1
 			if T(1, j) < 0
 				min_val = Inf;
@@ -103,7 +144,7 @@ function [T b_idx ind x] = tableau(T, b_idx, m, n, print)
 							i_pivot = i;
 						elseif theta == min_val
 							# Fica com o menor índice no caso de igualdade.
-							if b_idx(i - 1) < b_idx(i_pivot - 1)
+							if B(i - 1) < B(i_pivot - 1)
 								min_val = theta;
 								i_pivot = i;
 							endif
@@ -122,23 +163,24 @@ function [T b_idx ind x] = tableau(T, b_idx, m, n, print)
 			endif
 		endfor
 	
-		# Testa se encontrado solução ótima.
+		# Testa se solução ótima foi encontrada.
 		if j_pivot == 0
 			ind = 0;
-			x = zeros(n, 1);
-			for i = 1:m
-				x(b_idx(i)) = T(i + 1, 1);
-			endfor
+			x = get_solution(T, B, m, n);
+			
+			if print
+				print_tableau(T, B, m, n, 0, 0, iter);
+			endif
+			
 			return;
 		endif
-
-		# Imprime o tableau se aplicável.
+		
 		if print
-			print_tableau(T, b_idx, m, n, i_pivot, j_pivot);
+			print_tableau(T, B, m, n, i_pivot, j_pivot, iter++);
 		endif
-	
+		
 		# Atualiza o vetor com os índices das variáveis básicas.
-		b_idx(i_pivot - 1) = j_pivot - 1;
+		B(i_pivot - 1) = j_pivot - 1;
 
 		# Pivota e continua.
 		T = pivot(T, i_pivot, j_pivot, m + 1, n + 1);
@@ -146,8 +188,11 @@ function [T b_idx ind x] = tableau(T, b_idx, m, n, print)
 endfunction
 
 
-function print_tableau(T, b_idx, m, n, i_pivot, j_pivot)
-	T
+function x = get_solution(T, B, m, n)
+	x = zeros(n, 1);
+	for i = 1:m
+		x(B(i)) = T(i + 1, 1);
+	endfor
 endfunction
 
 
@@ -161,44 +206,7 @@ function T = pivot(T, i_pivot, j_pivot, m, n)
 endfunction
 
 
-function [T b_idx m] = remove_artificial(T, b_idx, m, n)
-	
-	# Remove variáveis artificiais
-	T(:, n+2:n+m+1) = [];
-	
-	# Procura e remove restrições redundantes.
-	remove_idx = []; 
-	for i = 1:m
-		if b_idx(i) > n
-			if is_zero(T(i + 1, 2:n+1))
-				remove_idx(end + 1) = i + 1;
-				m--;
-			endif
-		endif
-	endfor
-	T(remove_idx, :) = [];
-	b_idx(remove_idx - 1) = [];
-	
-	# Remove variáveis artificiais da base.
-	for i = 1:m
-		if b_idx(i) > n
-			for j = 2:n+1
-				if T(i + 1, j) != 0
-					T = pivot(T, i + 1, j, m + 1, n + 1);
-					b_idx(i) = j - 1;
-				endif
-			endfor
-		endif
-	endfor
-endfunction
-
-
-function bool = is_zero(v)
-	for i = 1:length(v)
-		if v(i) != 0
-			bool = false;
-			return;
-		endif
-	endfor
-	bool = true;
+function print_tableau(T, B, m, n, i_pivot, j_pivot, iter)
+	iter
+	T
 endfunction
